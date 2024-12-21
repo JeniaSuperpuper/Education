6322f097f1f52510c5e57b637ab14a7466ac08bf
jenia <fgdg>
2024-12-21 23:55:01 +0300

    валидация пароля

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -115,13 +115,14 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+from django.contrib.auth.password_validation import MinimumLengthValidator, CommonPasswordValidator
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth.password_validation import validate_password
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
     class Meta:
         model = CustomUser
         fields = '__all__'
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+            username = validated_data['username'],
+            email = validated_data['email'],
+            role = validated_data['role'],
+            avatar = validated_data.get('avatar', None)
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
