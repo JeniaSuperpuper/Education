66ae234b72c79fa0ccb1c7f797caeb648e3b4e95
user <oderovaleksandr@gmail.com>
2024-12-26 12:36:52 +0300

    Fixed modal &  AddNewFunc

--- frontend/src/components/modal.jsx
+++ frontend/src/components/modal.jsx
@@ -1,11 +1,34 @@
-const Modal = ({active, setActive, children}) => {
-    return (
-        <div className={active ? "modal active": "modal"} onClick={() => setActive(false)}>
-            <div className={active ? "modal__content active": "modal__content"} onClick={e => e.stopPropagation()}>
-                {children}
-            </div>
-        </div>
-    );
+import  { useEffect } from "react";
+
+const Modal = ({ active, setActive, children }) => {
+  const handleKeyDown = (e) => {
+    if (e.key === "Escape") {
+      setActive(false);
+    }
+  };
+
+  useEffect(() => {
+    if (active) {
+      window.addEventListener("keydown", handleKeyDown);
+    }
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [active]); 
+
+  return (
+    <div className={active ? "modal active" : "modal"}>
+      <div
+        className={active ? "modal__content active" : "modal__content"}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <p className="modal__close-btn" onClick={() => setActive(false)}>
+        &#x2715;
+        </p>
+        {children}
+      </div>
+    </div>
+  );
 };
 
-export default Modal
+export default Modal;
--- frontend/src/components/registration.jsx
+++ frontend/src/components/registration.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function RegistrationForm() {
   // Состояние для хранения данных формы
   const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirm_password: '',
+    username: "",
+    email: "",
+    password: "",
+    confirm_password: "",
   });
 
   // Состояние для отображения сообщений об ошибках или успехе
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   // Обработчик изменения полей формы
   const handleChange = (e) => {
@@ -27,93 +27,102 @@ export default function RegistrationForm() {
 
     // Проверка на совпадение паролей
     if (formData.password !== formData.confirm_password) {
-      setMessage('Пароли не совпадают');
+      setMessage("Пароли не совпадают");
       return;
     }
 
     try {
       // Отправка данных на сервер с помощью fetch
-      const response = await fetch('http://127.0.0.1:8000/api/v1/users/register/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData), // Преобразуем данные в JSON
-      });
+      const response = await fetch(
+        "http://127.0.0.1:8000/api/v1/users/register/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData), // Преобразуем данные в JSON
+        }
+      );
 
       // Обработка ответа от сервера
       const data = await response.json();
       if (response.ok) {
-        setMessage('Регистрация прошла успешно!');
+        setMessage("Регистрация прошла успешно!");
         // Очищаем форму после успешной отправки
         setFormData({
-          username: '',
-          email: '',
-          password: '',
-          confirm_password: '',
+          username: "",
+          email: "",
+          password: "",
+          confirm_password: "",
         });
       } else {
         setMessage(`Ошибка: ${data.message}`);
       }
     } catch (error) {
-      setMessage('Произошла ошибка при отправке данных');
+      setMessage("Произошла ошибка при отправке данных");
     }
   };
 
   return (
     <div>
-      <h1 className='form__auth title'>Register</h1>
-      <form className='form__auth form' onSubmit={handleSubmit}>
-        <div className='form__auth div'>
-            <label>
+      <h1 className="form__auth title">Register</h1>
+      <form className="form__auth form" onSubmit={handleSubmit}>
+        <div className="form__auth div">
+          <label>
             <p>Username</p>
-            <input className='form__auth input'
+            <input
+              className="form__auth input"
               type="text"
               name="username"
               value={formData.username}
               onChange={handleChange}
               required
             />
-            </label>
+          </label>
         </div>
-        <div className='form__auth div'>
-            <label>
+        <div className="form__auth div">
+          <label>
             <p>Email</p>
-            <input className='form__auth input'
+            <input
+              className="form__auth input"
               type="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
               required
             />
-            </label>
+          </label>
         </div>
-        <div className='form__auth div'>
-            <label>
+        <div className="form__auth div">
+          <label>
             <p>Password</p>
-            <input className='form__auth input'
+            <input
+              className="form__auth input"
               type="password"
               name="password"
               value={formData.password}
               onChange={handleChange}
               required
             />
-            </label>
+          </label>
         </div>
-        <div className='form__auth div'>
-            <label>
+        <div className="form__auth div">
+          <label>
             <p>Confirm password</p>
-            <input className='form__auth input'
+            <input
+              className="form__auth input"
               type="password"
               name="confirm_password"
               value={formData.confirm_password}
               onChange={handleChange}
               required
             />
-            </label>
+          </label>
         </div>
-        {message && <p className='form__auth info'>{message}</p>}
-        <button className='form__auth button' type="submit">Register</button>
+        {message && <p className="form__auth info">{message}</p>}
+        <button className="form__auth button" type="submit">
+          Register
+        </button>
       </form>
     </div>
   );
